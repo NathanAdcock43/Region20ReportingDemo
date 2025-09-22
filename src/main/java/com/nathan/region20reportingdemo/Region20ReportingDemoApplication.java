b9e7638c78b07711cfcf83e041012ef0f8d2a150
Nathan Adcock <nathan.adcock43@gmail.com>
2025-09-21 19:57:16 -0500

    Initial import of region20 project

---
+++ src/main/java/com/nathan/region20reportingdemo/Region20ReportingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.region20reportingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Region20ReportingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Region20ReportingDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nathan/region20reportingdemo/Region20ReportingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.region20reportingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Region20ReportingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
